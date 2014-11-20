22deae3a05d4b279be1bf134c1eb543a548d36d2
Slyshyk Oleksiy <Slyshyk Oleksiy@localhost>
2014-11-20 08:01:13 +0200

    Fix file header

--- xmlrpctest/testClient/src/main.cpp
+++ xmlrpctest/testClient/src/main.cpp
@@ -1,4 +1,9 @@
-/* file : main.cpp created : 11.01.2012 author : Slyshyk Oleksiy (alex312@meta.ua) */
+/* file : main.cpp
+ * created : 11.01.2012
+ * author : Slyshyk Oleksiy (alex312@meta.ua)
+*/
+
+
 #include <QApplication>
 #include "mainwindow.hpp"
 int main( int argc, char *argv[] )
--- xmlrpctest/testClient/src/mainwindow.cpp
+++ xmlrpctest/testClient/src/mainwindow.cpp
@@ -1,7 +1,9 @@
 /*
- * file : mainwindow.cpp created : 11.01.2012 author : Slyshyk Oleksiy
- * (alex312@meta.ua
+ * file : mainwindow.cpp
+ * created : 11.01.2012
+ * author : Slyshyk Oleksiy (alex312@meta.ua)
  */
+
 #include <QDateTime>
 #include <QApplication>
 #include <qtxmlrpc.hpp>
